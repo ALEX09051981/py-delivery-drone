f4e91b338d46f5515825320e0cfa97eaa2d96513
Alexey <alopar632@gmail.com>
2025-06-10 08:58:27 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -9,10 +9,10 @@ class Cargo:
 
 class BaseRobot:
     def __init__(
-            self,
-            name: str,
-            weight: int,
-            coords: list[int] = None
+        self,
+        name: str,
+        weight: int,
+        coords: list[int] = None
     ) -> None:
         self.name = name
         self.weight = weight
@@ -31,15 +31,18 @@ class BaseRobot:
         self.coords[0] -= step
 
     def get_info(self) -> str:
-        return f"Robot: {self.name}, Weight: {self.weight}"
+        return (
+            f"Robot: {self.name}, "
+            f"Weight: {self.weight}"
+        )
 
 
 class FlyingRobot(BaseRobot):
     def __init__(
-            self,
-            name: str,
-            weight: int,
-            coords: list[int] = None
+        self,
+        name: str,
+        weight: int,
+        coords: list[int] = None
     ) -> None:
         coords = coords if coords is not None else [0, 0, 0]
         super().__init__(name, weight, coords)
@@ -62,9 +65,11 @@ class DeliveryDrone(FlyingRobot):
     ) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
-        self.current_load = None
-        if current_load:
-            self.hook_load(current_load)
+
+        if current_load and current_load.weight <= max_load_weight:
+            self.current_load = current_load
+        else:
+            self.current_load = None
 
     def hook_load(self, cargo: Cargo) -> None:
         if self.current_load is None and cargo.weight <= self.max_load_weight:
